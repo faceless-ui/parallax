e1d4448630d7ae48f78b5c8c758914a389fcb7ae
Jacob Fletcher <jacobsfletch@gmail.com>
2019-11-13 21:22:09 -0500

    installs and retrofits @trbl/react-window-info and @trbl/react-scroll-position

--- src/ParallaxProvider/index.js
+++ src/ParallaxProvider/index.js
@@ -1,109 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { WindowInfoProvider, WindowInfoContext } from '@trbl/react-window-info';
+import { ScrollPositionProvider, ScrollPositionContext } from '@trbl/react-scroll-position';
 import ParallaxContext from './context';
 import defaultClassPrefix from '../defaultClassPrefix';
 
-class ParallaxProvider extends Component {
-  constructor(props) {
-    super(props);
+const ParallaxProvider = (props) => {
+  const {
+    children,
+    classPrefix,
+  } = props;
 
-    this.state = {
-      scrollAnimationScheduled: false,
-      resizeAnimationScheduled: false,
-      scrollPos: {
-        x: 0,
-        y: 0,
-      },
-      windowInfo: {
-        width: 0,
-        height: 0,
-      },
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.requestScrollAnimation);
-    window.addEventListener('resize', this.requestResizeAnimation);
-    window.addEventListener('orientationchange', this.updateWindowInfoWithTimeout);
-    this.updateScrollPos();
-    this.updateWindowInfo();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.requestAnimation);
-    window.removeEventListener('resize', this.requestAnimation);
-    window.removeEventListener('orientationchange', this.updateWindowInfoWithTimeout);
-  }
-
-  updateWindowInfoWithTimeout = () => {
-    setTimeout(() => {
-      this.updateSizes();
-    }, 500);
-  }
-
-  updateScrollPos = () => {
-    const { scrollPos } = this.state;
-
-    this.setState({
-      scrollAnimationScheduled: false,
-      scrollPos: {
-        ...scrollPos,
-        x: window.pageXOffset,
-        y: window.pageYOffset,
-      },
-    });
-  };
-
-  updateWindowInfo = () => {
-    this.setState({
-      resizeAnimationScheduled: false,
-      windowInfo: {
-        width: window.innerWidth,
-        height: window.innerHeight,
-      },
-    });
-  }
-
-  requestScrollAnimation = () => {
-    const { scrollAnimationScheduled } = this.state;
-    if (!scrollAnimationScheduled) {
-      requestAnimationFrame(this.updateScrollPos);
-      this.setState({ scrollAnimationScheduled: true });
-    }
-  }
-
-  requestResizeAnimation = () => {
-    const { resizeAnimationScheduled } = this.state;
-    if (!resizeAnimationScheduled) {
-      requestAnimationFrame(this.updateWindowInfo);
-      this.setState({ resizeAnimationScheduled: true });
-    }
-  }
-
-  render() {
-    const {
-      children,
-      classPrefix,
-    } = this.props;
-
-    const {
-      scrollPos,
-      windowInfo,
-    } = this.state;
-
-    const parallaxContext = {
-      windowInfo,
-      scrollPos,
-      classPrefix: classPrefix || defaultClassPrefix,
-    };
-
-    return (
-      <ParallaxContext.Provider value={parallaxContext}>
-        {children}
-      </ParallaxContext.Provider>
-    );
-  }
-}
+  return (
+    <WindowInfoProvider>
+      <WindowInfoContext.Consumer>
+        {(windowInfoContext) => {
+          const { windowInfo } = windowInfoContext;
+          return (
+            <ScrollPositionProvider>
+              <ScrollPositionContext.Consumer>
+                {(scrollPositionContext) => {
+                  const { scrollPos } = scrollPositionContext;
+                  const parallaxContext = {
+                    scrollPos,
+                    windowInfo,
+                    classPrefix: classPrefix || defaultClassPrefix,
+                  };
+                  return (
+                    <ParallaxContext.Provider value={parallaxContext}>
+                      {children}
+                    </ParallaxContext.Provider>
+                  );
+                }}
+              </ScrollPositionContext.Consumer>
+            </ScrollPositionProvider>
+          );
+        }}
+      </WindowInfoContext.Consumer>
+    </WindowInfoProvider>
+  );
+};
 
 ParallaxProvider.defaultProps = {
   classPrefix: '',
